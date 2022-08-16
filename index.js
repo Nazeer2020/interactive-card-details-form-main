6344d045b7594c7611f293eae194a8bcacabd47d
Nazeer Haidari <nazeer.haidari@gmail.com>
2022-08-16 21:36:01 +0430

    sixth commit

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ const numberInputEl = document.querySelector(".number-input");
 const monthInputEl = document.querySelector(".month-input");
 const yearInputEl = document.querySelector(".year-input");
 const cvcInputEl = document.querySelector(".cvc-input");
+const btnContinue = document.querySelector(".btn-continue");
 
 const formValidation = () => {
+  let num = 0;
   if (!Number(numberInputEl.value)) {
     numberInputEl.setAttribute("data-error", "true");
     numberInputEl.closest("span").setAttribute("data-number", "true");
   } else {
     numberInputEl.setAttribute("data-error", "false");
     numberInputEl.closest("span").setAttribute("data-number", "false");
+    num++;
   }
 
   if (cvcInputEl.value === "") {
@@ -24,6 +27,7 @@ const formValidation = () => {
   } else {
     cvcInputEl.setAttribute("data-error", "false");
     cvcInputEl.closest("span").setAttribute("data-cvc", "false");
+    num++;
   }
 
   if (yearInputEl.value === "") {
@@ -32,6 +36,7 @@ const formValidation = () => {
   } else {
     yearInputEl.setAttribute("data-error", "false");
     yearInputEl.closest("span").setAttribute("data-yy", "false");
+    num++;
   }
 
   if (monthInputEl.value === "") {
@@ -40,10 +45,22 @@ const formValidation = () => {
   } else {
     monthInputEl.setAttribute("data-error", "false");
     monthInputEl.closest("span").setAttribute("data-mm", "false");
+    num++;
   }
+
+  return num;
 };
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  formValidation();
+  const result = formValidation();
+  if (result === 4) {
+    console.log("hey");
+    document.querySelector(".complete-form-container").classList.add("show");
+    document.querySelector(".form-sub-container").classList.add("hide");
+  }
+});
+
+btnContinue.addEventListener("click", () => {
+  location.reload();
 });
